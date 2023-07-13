4a3acf576cfd956208405332222945f572f6deb8
DevStar0802 <ssantiagogutierrezzz@gmail.com>
2023-07-13 12:06:07 -0700

    Created models folder with user model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create user model
+const studentSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            max_length: 50,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            max_length: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            max_length: 60,
+        },
+        password: { //TODO: apply appropriate validators for password
+            type: String,
+            required: true,
+            max_length: 50,
+        }
+    },
+    { //TODO: check if this is necessary to add
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+const Student = model('student', studentSchema);

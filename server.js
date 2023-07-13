3cbccf96c1d304c65d16b35e87c82cdabac6ed81
DevStar0802 <ssantiagogutierrezzz@gmail.com>
2023-07-13 11:43:52 -0700

    update express routes to render index.html

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const routes = require('./controllers');
 const app = express()
 const port = process.env.PORT || 3001
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server Live, app listening on port ${port}`)
